8eeb01002f1d4110b6a2ec9095cb77e8a285a969
Yuri <yurifdev@gmail.com>
2023-06-04 13:39:11 -0300

    Comenting more examples

--- Cap1/objetoLiteral.js
+++ Cap1/objetoLiteral.js
@@ -24,3 +24,72 @@ exibir2PorBaixoDosPanos();
 
 const exibirNome = exibir2PorBaixoDosPanos.bind(user);
 exibirNome();
+
+
+/*
+OUTRAS MANEIRAS PARA DITAR CONTEXTOS
+________________________________CALL________________________________
+
+EX1:______________________
+function exibeInfos() {
+  console.log(this.nome, this.email)
+}
+
+const user = {
+  nome: 'Mariana',
+  email: 'm@m.com'
+}
+
+exibeInfos.call(user)
+
+EX2:_____________________
+function User(nome, email) {
+  this.nome = nome
+  this.email = email
+  
+  this.exibeInfos = function(){
+    console.log(this.nome, this.email)
+  }
+}
+
+const newUser = new User('mariana', 'm@m.com')
+
+const outroUser = {
+  nome: 'Rodrigo',
+  email: 'r@r.com'
+}
+
+newUser.exibeInfos() //mariana m@m.com
+newUser.exibeInfos.call(outroUser) //Rodrigo r@r.com
+
+EX3:_____________________
+function exibeMensagem(nome, email) {
+ console.log(`usuário: ${nome}, email ${email}`)
+}
+ const user = {
+ nome: 'Mariana',
+ email: 'm@m.com',
+ executaFuncao: function(fn) {
+   fn.call(user, this.nome, this.email)
+ }
+}
+
+user.executaFuncao(exibeMensagem) //usuário: Mariana, email m@m.com
+
+___________________________________APPLY___________________________________
+function exibeMensagem(nome, email) {
+ console.log(`usuário: ${nome}, email ${email}`)
+}
+ const user = {
+ nome: 'Mariana',
+ email: 'm@m.com',
+ executaFuncao: function(fn) {
+   fn.apply(user, [this.nome, this.email])
+ }
+}
+ user.executaFuncao(exibeMensagem) //usuário: Mariana, email m@m.com
+
+Usando arrays, é possível passar os argumentos via variável 
+ou até mesmo usando a propriedade arguments que existe internamente em todo objeto.
+
+ */
