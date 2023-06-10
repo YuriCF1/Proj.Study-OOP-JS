1ccf4456a5a6c6fa57733d0c9208b439ab9db140
Yuri <yurifdev@gmail.com>
2023-06-09 21:28:36 -0300

    Using static mehtods

--- Cap3/User.js
+++ Cap3/User.js
@@ -1,4 +1,4 @@
-// _______________________Criando user padrão_______________________ 
+// _______________________Criando user padrão_______________________
 export default class User {
   constructor(nome, email, nascimento, role, ativo = true) {
     this.nome = nome;
@@ -18,3 +18,23 @@ export default class User {
 // console.log(novoUser.exibirInfos());
 
 // console.log(User.prototype.isPrototypeOf(novoUser)); //Chegando a cadeia de protótipo de novoUser
+
+//___________________________Métodos estáticos___________________________
+class User2 {
+  constructor() {
+    this.nome = "Camila";
+    this.email = "c@c.com";
+    this.cpf = "12312312312";
+  }
+  exibirInfos() {
+    return `${this.nome}, ${this.email}, ${this.cpf}`;
+  }
+
+  static exibeNome(nome) {
+    return nome;
+  }
+}
+
+const novoUser = new User2("Carol", "c@c.com", "12312312312");
+const nomeUser = novoUser.nome;
+console.log(User2.exibeNome(nomeUser)); //Camila
