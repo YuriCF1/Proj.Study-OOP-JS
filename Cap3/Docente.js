cc703e4e81b21ddb224a1d183b1e409675eee12e
Yuri <yurifdev@gmail.com>
2023-06-10 18:47:44 -0300

    Creating class of professors

---
+++ Cap3/Docente.js
@@ -0,0 +1,20 @@
+//OBS: Todas as classes seguem no modo restrito
+//"use strict"
+
+import User from "./User.js";
+
+class Docente extends User {
+  constructor(nome, email, nascimento, role = "docente", ativo = true) {
+    super(nome, email, nascimento, role, ativo);
+  }
+
+  aprovaEstudante(estudante, curso) {
+    return `Estudante ${estudante}, passou no curso ${curso}`;
+  }
+}
+
+
+const novoDocente = new Docente("Maria", "m@m.com", "2001-01-01")
+console.log(novoDocente);
+console.log(novoDocente.exibirInfos());
+console.log(novoDocente.aprovaEstudante);
