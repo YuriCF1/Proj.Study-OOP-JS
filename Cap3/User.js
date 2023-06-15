436622ace6c10fccfccd00e9ddf7bf6e97f4bce0
Yuri <yurifdev@gmail.com>
2023-06-15 19:03:24 -0300

    Taking notes and testing polymorphism

--- Cap3/User.js
+++ Cap3/User.js
@@ -57,7 +57,7 @@ export default class User {
     // return objUser
     // return `${this.#nome}, ${this.#email}`;
     // return `${objUser.nome}, ${objUser.email}`;
-    return `${this.nome}, ${this.email}`;
+    return `${this.nome}, ${this.email}, ${this.role}`;
   }
 }
 
@@ -86,3 +86,83 @@ export default class User {
 // const novoUser = new User2("Carol", "c@c.com", "12312312312");
 // const nomeUser = novoUser.nome;
 // console.log(User2.exibeNome(nomeUser)); //Camila
+
+
+// ___________________________________________SALVANDO NOME E SOBRENOME____________________________________________
+/*
+  default class User {
+ #nome
+ #sobrenome
+ // restante das propriedades
+
+  constructor (nome, sobrenome, email, nascimento, role, ativo = true) {
+   this.#nome = nome
+   this.#sobrenome = sobrenome
+   // restante das propriedades
+
+    set nome(novoNome) {
+   if (novoNome === '') {
+     throw new Error('formato não válido')
+   }
+   let [nome, ...sobrenome] = novoNome.split(" ")
+   sobrenome = sobrenome.join(' ')
+   this.#nome = nome
+   this.#sobrenome = sobrenome
+ }
+
+  get nome() {
+   return this.#nome
+ }
+
+ get sobrenome() {
+   return this.#sobrenome
+ }
+
+ const novoUser = new User('Juliana', 'Souza', 'j@j.com', '2021-01-01')
+console.log(novoUser.nome) //'Juliana'
+novoUser.nome = 'Juliana Silva Souza'
+console.log(novoUser.nome) //'Juliana'
+console.log(novoUser.sobrenome) //'Silva Souza'
+
+ get nome() {
+   return `${this.#nome} ${this.#sobrenome}`
+ }
+
+ Explicação:
+
+let [nome, ...sobrenome] = novoNome.split(" ");:
+
+A variável novoNome é uma string que contém o nome completo.
+O método split(" ") é chamado em novoNome, dividindo a string em um array de substrings, usando o espaço como separador. Cada parte do nome é um elemento do array resultante.
+O operador de desestruturação [nome, ...sobrenome] é usado para extrair o primeiro elemento do array (que será o nome) e armazená-lo na variável nome, e todos os elementos restantes do array (que serão os sobrenomes) são armazenados na variável sobrenome como um array.
+sobrenome = sobrenome.join(' ');:
+
+A variável sobrenome é um array que contém os sobrenomes.
+O método join(' ') é chamado em sobrenome, juntando todos os elementos do array em uma única string, separando-os por um espaço. Isso combina os sobrenomes em uma única string.
+
+// ___________________________________________DIFERENÇA ENTRE FUNÇÕES____________________________________________
+
+/*
+class User {
+ _nome = '';
+
+ setNome(nome) {
+   this._nome = nome;
+ }
+
+ getNome() {
+   return this._nome;
+ }
+}
+
+-----FUNÇÕES-------
+const nome = novoUser.getNome() //getter
+novoUser.setNome('Pedro') //setter
+novoUser.exibeInfos() // método normal
+
+-----SETTERS GETTERS------
+const nome = novoUser.nome //getter
+novoUser.nome = 'Pedro' //setter
+novoUser.exibeInfos() // método normal
+*/
+
